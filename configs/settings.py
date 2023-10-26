a5b76f7e3d1942d082eccaa3007e015f855283be
regvud <silveron.regvud@gmail.com>
2023-10-26 16:05:50 +0300

    clown

--- configs/settings.py
+++ configs/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 import socket
+import pymysql
 from pathlib import Path
 
 from .additional_configs import *
 
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 socket.getaddrinfo('127.0.0.1', 8000)
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('secret_key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

df7137512fa469da503b9001e86c538d274380a4
regvud <silveron.regvud@gmail.com>
2023-10-28 14:43:49 +0300

    docker setup successful 2

--- backend/configs/additional_configs/rest_config.py
+++ backend/configs/additional_configs/rest_config.py
@@ -1,4 +1,7 @@
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.PagePagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',

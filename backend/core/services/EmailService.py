49ca061e09b86833c07e839587d87080e4dc56d1
regvud <silveron.regvud@gmail.com>
2023-10-30 13:49:07 +0200

    wtf

---
+++ backend/core/services/EmailService.py
@@ -0,0 +1,21 @@
+import os
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.views import static
+
+
+class EmailService:
+    @staticmethod
+    def __send_email(to: str, template_name: str, context={}, subject=""):
+        template = get_template(template_name)
+        html_content = template.render(context)
+        msg = EmailMultiAlternatives(
+            subject, from_email=os.environ.get("EMAIL_HOST_USER"), to=[to]
+        )
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+    @classmethod
+    def test_email(cls):
+        cls.__send_email("skilide.skilide@gmail.com", "test.html", {}, "Subject")

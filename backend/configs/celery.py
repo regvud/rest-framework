559bb613e0953e603fe1320ee4a5677740997ecc
regvud <silveron.regvud@gmail.com>
2023-11-06 18:51:13 +0200

    nginx provided

--- backend/configs/celery.py
+++ backend/configs/celery.py
@@ -6,6 +6,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings")
 
 app = Celery("settings")
 
-app.config_from_object("django.conf.settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
